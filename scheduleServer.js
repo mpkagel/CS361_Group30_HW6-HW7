3b7092553ccb4af6628276c09cdf2b0001670d62
jcostello93 <costejoh@oregonstate.edu>
2018-03-06 16:35:28 +0100

    Update scheduleServer.js
    Edited code to add task. 
    Wrote code to update and delete tasks.

--- scheduleServer.js
+++ scheduleServer.js
@@ -14,9 +14,10 @@ app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 app.set('port', 5047);
 app.use(express.static('public'));
-var credentials = require('./credentials.js');
+//var credentials = require('./credentials.js');
 var dataDef = require('./dataDefinition.js');
 var testData = require('./testDataCreation.js')
 
@@ -31,7 +32,6 @@ app.get('/', function(req, res, next) {
     //context.maprequest = "https://maps.googleapis.com/maps/api/js?&key=" + credentials.mapKey + "&callback=initMap";
 
     context.results = scheduleData.profiles[0].Schedule.tasks;
-
     res.render('home', context);
 }); 
 
@@ -39,12 +39,50 @@ app.post('/', function(req, res) {
   var context = {};
   if (req.body['Add Item']) {
     newTask = new dataDef.Task(req.body.name, req.body.date, req.body.time,
-    	req.body.address, req.body.city, req.body.state, req.body.recurring);
-  
+    	req.body.address, req.body.city, req.body.state, req.body.recurring);   
     scheduleData.profiles[0].Schedule.tasks.push(newTask);
+    context.results = scheduleData.profiles[0].Schedule.tasks;
+    res.render('home', context);
 
   }
 });
+
+app.get('/update/:id', function(req, res){
+	callbackCount = 0;
+	var context = {};
+	context.jsscripts = ["updatetask.js"];
+	console.log(req.params.id);
+	id = req.params.id; 
+	context = scheduleData.profiles[0].Schedule.tasks[id]; 
+	context.id = id; 
+	res.render('update-task', context);
+});
+
+app.put('/tasks/:id', function(req, res){
+	var context = {};
+	scheduleData.profiles[0].Schedule.tasks[id].name = req.body.name;
+	scheduleData.profiles[0].Schedule.tasks[id].date = req.body.date;
+	scheduleData.profiles[0].Schedule.tasks[id].time = req.body.time;
+	scheduleData.profiles[0].Schedule.tasks[id].Location.address = req.body.address;
+	scheduleData.profiles[0].Schedule.tasks[id].Location.city = req.body.city;
+	scheduleData.profiles[0].Schedule.tasks[id].Location.state = req.body.state;
+
+  context.results = scheduleData.profiles[0].Schedule.tasks;
+  res.send(null);	
+});
+
+app.put('/delete/:id', function(req, res){
+
+	id = req.params.id;
+	callbackCount = 0;
+	var context = {};
+	scheduleData.profiles[0].Schedule.tasks.splice(id, 1); 
+	context = scheduleData.profiles[0].Schedule.tasks[id]; 
+	context.id = req.params.id; 
+	res.send(null); 
+});
+
+
 
 app.use(function(req,res) {
   	res.status(404);
