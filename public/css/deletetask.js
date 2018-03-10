b9e7bb25f7b6eb5910dcd0eeffbfa8b5a5e2c932
mpkagel <kagelm@oregonstate.edu>
2018-03-10 15:42:17 -0800

    Delete deletetask.js

--- public/css/deletetask.js
+++
@@ -1,9 +0,0 @@
-function deleteTask(id){
-    $.ajax({
-        url: '/delete/' + id,
-        type: 'PUT',
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
-};
